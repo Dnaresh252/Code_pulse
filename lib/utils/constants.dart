ec4da1e1964324eb4fef75bc4e0d2030244ead41
Dantina Yugandhar Naresh <142888100+Dnaresh252@users.noreply.github.com>
2025-09-05 21:18:40 +0530

    Updated for Api key

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,4 @@
 class ApiConstants {
-  static const String apiKey = 'AIzaSyDQHmTFLuRnrnvtOVuLdX3io9QHhoI1d6s';
+  static const String apiKey = "Paste Your api key";
   static const String modelName = 'gemini-2.0-flash';
 }
